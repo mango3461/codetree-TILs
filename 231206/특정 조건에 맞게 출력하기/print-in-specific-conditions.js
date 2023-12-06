445cf24efa8c7d30848b22323d86284c155e8b43
mango3461 <80249639+mango3461@users.noreply.github.com>
2023-12-06 23:20:22 +0900

    Title: 특정 조건에 맞게 출력하기, Time: 219ms, Memory: 45MB, Status: Passed - CodeTree

--- 231206/특정 조건에 맞게 출력하기/print-in-specific-conditions.js
+++ 231206/특정 조건에 맞게 출력하기/print-in-specific-conditions.js
@@ -1,9 +1,12 @@
-const fs=require('fs')
-let input=fs.readFileSync(0).toString().trim().split(' ').map(Number)
-let arr=[]
-for(let i=0;i<100;i++){
-    arr=input.filter(ele=> ele!==0).map(ele => {
-        return ele % 2 !== 0 ? ele + 3 : ele / 2;
-    })
+const fs = require('fs');
+let input = fs.readFileSync(0).toString().trim().split(' ').map(Number);
+let arr = input;
+
+let index = arr.findIndex(ele => ele === 0);
+
+if (index !== -1) {
+    arr.splice(index);
 }
-console.log(arr.join(' '))
+arr = arr.map(ele => ele % 2 !== 0 ? ele + 3 : ele / 2);
+
+console.log(arr.join(' '));
