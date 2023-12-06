30b1c817c5f862b0da8c96e50fceb5233f2e4957
mango3461 <80249639+mango3461@users.noreply.github.com>
2023-12-06 20:49:28 +0900

    Title: 숫자들의 배수, Time: 251ms, Memory: 45MB, Status: Passed - CodeTree

--- 231206/숫자들의 배수/multiple-of-numbers.js
+++ 231206/숫자들의 배수/multiple-of-numbers.js
@@ -1,8 +1,11 @@
 const fs=require('fs')
 let n=Number(fs.readFileSync(0).toString().trim())
 let arr=[];
-
+let cnt=0;
 for(let i=1;i<=10;i++){
     arr.push(n*i)
+    if((n*i)%5===0)cnt++
+    if(cnt===2) break;
+
 }
 console.log(arr.join(' '))
