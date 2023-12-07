5e1dfe7efb9d8856b2c28c57b2c768f864728f15
mango3461 <80249639+mango3461@users.noreply.github.com>
2023-12-07 23:13:50 +0900

    Title: 십의 자리 숫자, Time: 285ms, Memory: 13MB, Status: Wrong Answer - CodeTree

--- 231207/십의 자리 숫자/number-of-tens-digit.js
+++ 231207/십의 자리 숫자/number-of-tens-digit.js
@@ -3,6 +3,7 @@ let input=fs.readFileSync(0).toString().trim().split(' ')
 let arr=Array(10).fill(0)
 
 for(let i=0; i<100;i++){
+    if(input[i]===0)break;
     let x = parseInt(input[i] / 10)
     arr[x]++
 }
