d560ca9abc9a9c1f0af1cda063c7e6f6b96809e0
mango3461 <80249639+mango3461@users.noreply.github.com>
2023-12-08 19:19:38 +0900

    Title: 나눗셈의 나머지, Time: 205ms, Memory: 13MB, Status: Wrong Answer - CodeTree

---
+++ 231208/나눗셈의 나머지/remainder-of-division.js
@@ -0,0 +1,15 @@
+const fs=require('fs')
+let input=fs.readFileSync(0).toString().trim().split(' ').map(Number)
+let a=input[0]
+let b=input[1]
+let arr=Array(b).fill(0)
+let sum=0;
+for(let i=1;i<=5;i++){
+    arr[a%b]++
+    a = parseInt(a/b)
+}
+
+for(let i=0;i<b;i++){
+    if(arr[i] !== 0) sum += arr[i]**2
+}
+console.log(sum)
