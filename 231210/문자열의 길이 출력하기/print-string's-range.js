eecd8ecd7ffb24c52b665f4981975426b77d81ff
mango3461 <80249639+mango3461@users.noreply.github.com>
2023-12-10 20:18:24 +0900

    Title: 문자열의 길이 출력하기, Time: 221ms, Memory: 13MB, Status: Passed - CodeTree

--- 231210/문자열의 길이 출력하기/print-string's-range.js
+++ 231210/문자열의 길이 출력하기/print-string's-range.js
@@ -2,5 +2,5 @@ const fs=require('fs')
 let input=fs.readFileSync(0).toString().trim().split('\n')
 console.log(
     Number(input[0].length)+
-    Number(input[0].length)
+    Number(input[1].length)
     )
