ac2f1123be1c8804ea752fd9fb681212472d502b
mango3461 <80249639+mango3461@users.noreply.github.com>
2023-12-16 03:16:23 +0900

    Title: 특정 문자로 끝나는 문자열, Time: 305ms, Memory: 13MB, Status: Passed - CodeTree

--- 231216/특정 문자로 끝나는 문자열/string-ending-with-specific-character.js
+++ 231216/특정 문자로 끝나는 문자열/string-ending-with-specific-character.js
@@ -1,7 +1,11 @@
 const fs=require('fs')
 let input=fs.readFileSync(0).toString().trim().split('\n')
-
+let cnt=0;
 for(let i=0;i<10;i++){
     let str=input[i]
-    if(str[str.length-1]===input[10]) console.log(str)
+    if(str[str.length-1]===input[10]) {
+        console.log(str)
+        cnt++
+    }
 }
+if(cnt===0) console.log('None')
