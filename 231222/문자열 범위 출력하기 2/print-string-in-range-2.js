9a2f168d6f7d5e9f80bf5dad4997aba4f8e51d9d
mango3461 <80249639+mango3461@users.noreply.github.com>
2023-12-22 16:11:54 +0900

    Title: 문자열 범위 출력하기 2, Time: 237ms, Memory: 45MB, Status: Passed - CodeTree

--- 231222/문자열 범위 출력하기 2/print-string-in-range-2.js
+++ 231222/문자열 범위 출력하기 2/print-string-in-range-2.js
@@ -4,11 +4,14 @@ let n = Number(input[1])
 let len = input[0].length;
 let str = ''
 
-for(let i = len-1; i>=len-n;i--) {
-    str +=input[0][i]
-}
+
 if(len<n) {
-    console.log(input[0])
+    for(let i = len-1; i>=0; i--){
+        str +=input[0][i]
+    }
 }else {
-    console.log(str)
+    for(let i = len-1; i>=len-n;i--) {
+        str +=input[0][i]
+    }
 }
+    console.log(str)
