18e8ee7d237c737d0d7728003697804eb384bd34
mango3461 <80249639+mango3461@users.noreply.github.com>
2023-12-23 23:33:49 +0900

    Title: 짝수 번째만 거꾸로 출력, Time: 209ms, Memory: 13MB, Status: Passed - CodeTree

--- 231223/짝수 번째만 거꾸로 출력/print-only-even-numbers-backwards.js
+++ 231223/짝수 번째만 거꾸로 출력/print-only-even-numbers-backwards.js
@@ -1,8 +1,11 @@
 const fs =require('fs')
 let input = fs.readFileSync(0).toString().trim();
 let len = input.length;
+let n = 0;
+if(len%2===0) n=len-1;
+else n = len-2;
 let str=''
-for(let i =len-1; i>=0;i-=2) {
+for(let i =n; i>=0;i-=2) {
     str+=input[i]
 }
 console.log(str)
